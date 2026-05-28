29c7a09637b6ebf2e9d4ef5074b5e70c3e4f4bc2
riyaladhar07-eng <riyaladhar07@gmail.com>
2026-05-28 18:05:30 +0530

    Register service worker on page load

--- script.js
+++ script.js
@@ -288,3 +288,18 @@ function aiEncouragement() {
         "chat-response"
     ).innerHTML = response;
 }
+
+if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker
+            .register("service-worker.js")
+            .then(() => {
+
+                console.log(
+                    "Service Worker Registered"
+                );
+            });
+    });
+}
